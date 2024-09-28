e5955d75b672582d65ac8a7e8059d65405b61980
its-ur-jobs-lars <120469885+its-ur-jobs-lars@users.noreply.github.com>
2024-09-28 10:44:20 +0000

    task1_complete!

---
+++ chapter1/Tasksheet111.java
@@ -0,0 +1,10 @@
+/** 
+ * The Tasksheet111 class implements an application that
+ * simply prints "Kamusta Mundo!" to standard output.
+ */
+
+public class Tasksheet111{
+     public static void main(String[] args) {
+        System.out.println("Kamusta Mundo!"); // Display the string.
+     }
+}
