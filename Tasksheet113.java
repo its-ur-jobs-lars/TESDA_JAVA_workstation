7a7bd39a2f65baad4c27e04afcc3899331880f50
its-ur-jobs-lars <120469885+its-ur-jobs-lars@users.noreply.github.com>
2024-10-08 08:59:21 +0000

    task3 complete

--- Tasksheet113.java
+++ Tasksheet113.java
@@ -1,5 +1,17 @@
 public class Tasksheet113 {
     public static void main(String[] args) {
+                
+        // Declare and initialize the primitives with specific values
+        byte zero = 0;             // Assigning byte value of 0 (to get '0' in the output)
+        short s = 2;            // Assigning short value of 2
+        int i = 1;              // Assigning int value of 1
+        char c = 'H';           // Assigning char value of 'H'
+        boolean bool = true;    // Assigning boolean value of true
         
+        // Concatenate the primitives into a string
+        String output = "" + c + "3" +  "11" + zero + " w" + zero + "r" + i + "d " + s + ".0 " + bool;
+        
+        // Print the output
+        System.out.println(output); // This prints: H3110 w0r1d 2.0 true
     }
 }
