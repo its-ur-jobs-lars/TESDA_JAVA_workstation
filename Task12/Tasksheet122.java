f02bd4dd0e0fc22b3df3aa13b486aae950776358
its-ur-jobs-lars <120469885+its-ur-jobs-lars@users.noreply.github.com>
2024-10-24 11:16:30 +0000

    Tasksheet122 done!

---
+++ Task12/Tasksheet122.java
@@ -0,0 +1,32 @@
+package Task12;
+
+    class Student {
+        private String firstName;
+        private String lastName;
+    
+        public Student(String firstName, String lastName) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+    
+        // Method to print the full name of the student
+        public void printFullName() {
+            System.out.println(firstName + " " + lastName);
+        }
+    }
+    
+    public class Tasksheet122 {
+        public static void main(String[] args) {
+            Student[] students = new Student[] {
+                new Student("Morgan", "Freeman"),
+                new Student("Brad", "Pitt"),
+                new Student("Kevin", "Spacey"),
+            };
+    
+            // Print the full name of each student
+            for (Student s : students) {
+                s.printFullName();
+            }
+        }
+    }
+    
