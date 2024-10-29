70086544df266dc1864eadcb84dd344ef839509d
its-ur-jobs-lars <120469885+its-ur-jobs-lars@users.noreply.github.com>
2024-10-29 09:55:21 +0000

    Task 125 done!

--- Task15/Tasksheet125.java
+++ Task15/Tasksheet125.java
@@ -1,5 +1,32 @@
 package Task15;
 
+class Point{
+    private double x;
+    private double y;
+
+    public Point(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public void print (){
+        System.out.println("(" + x + "," + y + ")");
+    }
+
+    // Scale method to bring the pointer closer to (0,0)
+
+    public void scale(){
+        x /= 2;
+        y /= 2;
+    }
+    }
+
 public class Tasksheet125 {
-    
+    public static void main(String[] args) {
+        Point point1 = new Point(32,32);
+        for (int i = 0; i <5; i++){
+            point1.scale();
+            point1.print();
+        }
+    }
 }
